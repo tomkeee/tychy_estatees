488f84190278320ec1e45b963282d8a2374298d2
Tomek Ścierski <tomasz6612@gmail.com>
2022-06-11 21:43:04 +0200

    fix cors

--- backend/django_api/django_api/settings.py
+++ backend/django_api/django_api/settings.py
@@ -41,16 +41,22 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "tychy",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = "django_api.urls"
